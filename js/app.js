9af5bf9ebcf9cf41eebdcfdd9e6acb728d504c21
23015162-Farhana <23015162@myrp.edu.sg>
2026-01-14 10:02:48 +0800

    Replace North Campus image with better food hall photo

--- js/app.js
+++ js/app.js
@@ -53,7 +53,7 @@ const sampleData = [
         id: 'north-ctn',
         campus: 'north',
         name: 'North Campus Food Hall',
-        img: 'https://images.unsplash.com/photo-1555939594-58d7cb561a1a?w=300&h=180&fit=crop',
+        img: 'https://images.unsplash.com/photo-1559827260-dc66d52bef19?w=300&h=180&fit=crop',
         stalls: [
             {
                 stallId: 'st4',
